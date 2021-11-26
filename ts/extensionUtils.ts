60a0303a60efd12d6172a8565d8e03fc9774d19e
Otto Allmendinger <mail@ttll.de>
2021-11-26 16:08:17 +0100

    Backport extensionUtils.gettext from v41
    The gettext convenience wrapper only became available in v41:
    https://gitlab.gnome.org/GNOME/gnome-shell/-/commit/1deb13e1aaabfd04b2641976a224b6fc2be3b9ec

--- ts/extensionUtils.ts
+++ ts/extensionUtils.ts
@@ -1,23 +1,39 @@
 import { Settings, File } from '@imports/Gio-2.0';
 
+
 interface ExtensionUtils {
-  initTranslations(): void;
+  initTranslations(domain?: string): void;
 
   getSettings(): Settings;
 
   getCurrentExtension(): {
     path: string;
-    metadata: any;
+    metadata: Record<string, unknown>;
     dir: File;
   };
 
-  gettext(msgid: string): string;
-
   openPrefs(): void;
+
+  gettext(str: string): string;
 }
 
 declare const imports: any;
 
-export default imports.misc.extensionUtils as ExtensionUtils;
+const extensionUtils = imports.misc.extensionUtils as ExtensionUtils;
 
-export const _ = imports.misc.extensionUtils.gettext;
+if (!extensionUtils.gettext) {
+  // backport from v41
+  // https://gitlab.gnome.org/GNOME/gnome-shell/-/blob/1deb13e1aaabfd04b2641976a224b6fc2be3b9ec/js/misc/extensionUtils.js#L117
+  const domain = extensionUtils.getCurrentExtension().metadata['gettext-domain'] as string;
+  extensionUtils.initTranslations(domain);
+  const gettextForDomain = imports.gettext.domain(domain);
+  if (gettextForDomain.gettext) {
+    Object.assign(extensionUtils, gettextForDomain);
+  } else {
+    logError(new Error(`could create gettextForDomain domain=${domain}`));
+  }
+}
+
+export default extensionUtils as ExtensionUtils;
+
+export const _ = extensionUtils.gettext;
